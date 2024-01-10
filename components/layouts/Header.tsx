850c5b702b55c24700e44afa6fb5d8cbf4033310
DengelLionel <dengelriveracajincho@gmail.com>
2024-01-10 12:44:43 -0500

    cambiando texto header

--- components/layouts/Header.tsx
+++ components/layouts/Header.tsx
@@ -119,15 +119,15 @@ const [error, setError] = useState(null)
           <ul className="flex space-x-4">
          
             <li>
-              <Link href="/"  className={`text-black2 transition-all duration-150 hover:text-green-500 ${currentPath === '/' ? 'text-green-500' : 'text-black2'}`}>
+              <Link href="/"  className={`text-black2 transition-all duration-150 hover:text-secundary2 ${currentPath === '/' ? 'text-text-secundary2' : 'text-black2'}`}>
                 INICIO
               </Link>
             </li>
             <li>
               <Link
                 href="#"
-                className={`text-black2 transition-all duration-150 hover:text-green-500 ${isSubMenuOpenD ?  'text-green-500' : 'text-black2'} ${
-                    currentPath === '/servicios' ?  'text-green-500' : 'text-black2'
+                className={`text-black2 transition-all duration-150 hover:text-text-secundary2 ${isSubMenuOpenD ?  'text-text-secundary2' : 'text-black2'} ${
+                    currentPath === '/servicios' ?  'text-text-secundary2' : 'text-black2'
                   }`}
                 onClick={(e) => {
                   e.preventDefault();
@@ -137,11 +137,11 @@ const [error, setError] = useState(null)
                 SERVICIOS ECOMMERCE
               </Link>
               {isSubMenuOpenD && (
-                <ul className="ml-4 absolute top-[60%] bg-green-500 p-2">
+                <ul className="ml-4 absolute top-[60%] bg-text-secundary2 p-2">
                   {/* Submenu */}
                   <li className='p-[8px]'>
-                    <Link href="/servicios/diseno-tienda-shopify"   className={`text-white transition-all duration-150 hover:text-gray-200 ${
-                        currentPath === '/servicios/diseno-tienda-shopify' ?  'text-gray-200' : 'text-black2'
+                    <Link href="/servicios/diseno-tienda-shopify"   className={`text-white transition-all duration-150 hover:text-text-secundary2 ${
+                        currentPath === '/servicios/diseno-tienda-shopify' ?  'text-text-secundary2' : 'text-black2'
                       }`}>
                      DISEÑO DE TIENDA SHOPIFY
                     </Link>
@@ -166,27 +166,27 @@ const [error, setError] = useState(null)
               )}
             </li>
             <li>
-              <Link href="/conocenos"    className={`text-black2 transition-all duration-150  hover:text-green-500 ${currentPath === '/conocenos' ? 'text-green-500' : 'text-black2'}`}>
+              <Link href="/conocenos"    className={`text-black2 transition-all duration-150  hover:text-text-secundary2 ${currentPath === '/conocenos' ? 'text-text-secundary2' : 'text-black2'}`}>
                 CONOCENOS
               </Link>
             </li>
             <li>
-              <span onClick={()=>Scroll("#contactanos")}    className={`text-black2 cursor-pointer transition-all duration-150  hover:text-green-500 ${currentPath === '/contactanos' ? 'text-green-500' : 'text-black2'}`}>
+              <span onClick={()=>Scroll("#contactanos")}    className={`text-black2 cursor-pointer transition-all duration-150  hover:text-text-secundary2 ${currentPath === '/contactanos' ? 'text-text-secundary2' : 'text-black2'}`}>
                 CONTACTANOS
               </span>
             </li>
             <li>
-              <Link href="https://www.tiktok.com/@creatiendaya"  title="TIKTOK"  className={`text-black2 transition-all duration-150 hover:text-green-500 ${currentPath === '/' ? 'text-green-500' : 'text-black2'}`}>
+              <Link href="https://www.tiktok.com/@creatiendaya"  title="TIKTOK"  className={`text-black2 transition-all duration-150 hover:text-text-secundary2 ${currentPath === '/' ? 'text-text-secundary2' : 'text-black2'}`}>
                <IconTiktok/>
               </Link>
             </li>
             <li>
-              <Link href="https://www.instagram.com/dengelrivera.dev/"  title="INSTAGRAM"  className={`text-black2 transition-all duration-150 hover:text-green-500 ${currentPath === '/' ? 'text-green-500' : 'text-black2'}`}>
+              <Link href="https://www.instagram.com/dengelrivera.dev/"  title="INSTAGRAM"  className={`text-black2 transition-all duration-150 hover:text-text-secundary2 ${currentPath === '/' ? 'text-text-secundary2' : 'text-black2'}`}>
                <IconInstagram/>
               </Link>
             </li>
             <li>
-              <Link href="https://www.facebook.com/creatiendaya/"  title="Facebook"  className={`text-black2 transition-all duration-150 hover:text-green-500 ${currentPath === '/' ? 'text-green-500' : 'text-black2'}`}>
+              <Link href="https://www.facebook.com/creatiendaya/"  title="Facebook"  className={`text-black2 transition-all duration-150 hover:text-text-secundary2 ${currentPath === '/' ? 'text-text-secundary2' : 'text-black2'}`}>
               <svg width="23" height="23" viewBox="0 0 23 23" fill="none" xmlns="http://www.w3.org/2000/svg">
 <path d="M11.5 0C5.15868 0 0 5.15888 0 11.5C0 17.8413 5.15868 23.0004 11.5 23.0004C17.8409 23.0004 23 17.8413 23 11.5C23 5.15888 17.8409 0 11.5 0ZM14.7502 7.25972C14.7502 7.31418 14.7286 7.3664 14.6901 7.4049C14.6516 7.44341 14.5994 7.46504 14.5449 7.46504L13.2376 7.46586C12.3751 7.46586 12.2174 7.80177 12.2174 8.46331V9.83567H14.4595C14.5184 9.83567 14.5747 9.86113 14.6135 9.90548C14.6523 9.94983 14.6704 10.0088 14.6632 10.0673L14.3439 12.5379C14.3375 12.5874 14.3133 12.6329 14.2758 12.6658C14.2383 12.6988 14.1901 12.7169 14.1402 12.7169H12.2174V18.8517C12.2174 18.9062 12.1958 18.9584 12.1573 18.9969C12.1187 19.0354 12.0665 19.057 12.0121 19.057H9.46117C9.40671 19.057 9.35449 19.0354 9.31598 18.9969C9.27748 18.9584 9.25585 18.9062 9.25585 18.8517V12.7169H7.32686C7.27241 12.7169 7.22018 12.6953 7.18168 12.6568C7.14317 12.6183 7.12154 12.5661 7.12154 12.5116V10.041C7.12154 9.98654 7.14317 9.93431 7.18168 9.89581C7.22018 9.8573 7.27241 9.83567 7.32686 9.83567H9.25585V8.21877C9.25585 6.07728 10.5518 4.74721 12.6385 4.74721C13.5355 4.74721 14.3283 4.81394 14.5718 4.84618C14.6212 4.8527 14.6665 4.87693 14.6993 4.91436C14.7321 4.95179 14.7502 4.99987 14.7502 5.04965V7.25972Z" fill="#ABA7C0"/>
 </svg>
@@ -198,7 +198,7 @@ const [error, setError] = useState(null)
       
     </nav>
     <div
-          className={`md:hidden absolute top-[80px] bg-green-500 p-[16px] w-[340px] transition-transform transform ${
+          className={`md:hidden absolute top-[80px] bg-text-secundary2 p-[16px] w-[340px] transition-transform transform ${
             isMenuOpen ? 'visible opacity-100 translate-y-0' : 'invisible opacity-0 -translate-y-4'
           }`}
         >
@@ -220,7 +220,7 @@ const [error, setError] = useState(null)
                 SERVICIOS ECOMMERCE
               </Link>
               {isSubMenuOpen && (
-                <ul className="ml-4  bg-green-500 p-2">
+                <ul className="ml-4  bg-text-secundary2 p-2">
                   {/* Submenu */}
                   <li className='p-[8px]'>
                     <Link href="/servicios/diseno-tienda-shopify" className="text-white">
