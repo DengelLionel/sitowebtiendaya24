2c4de2bb95a2b7eea9ee9416e3c203ebf145b4a8
DengelLionel <dengelriveracajincho@gmail.com>
2024-03-03 19:44:04 -0500

    ddd

--- components/ecommerce/ImagenEmpresas.tsx
+++ components/ecommerce/ImagenEmpresas.tsx
@@ -8,8 +8,8 @@ interface image{
 }
 const ImagenEmpresas = ({imagen,titulo,link}:image) => {
   return (
-    <Link  className='w-[300px]  h-[221px] ' href={link}>
-    <Image className='w-[300px]  h-[221px] xl:scale-90 transition-all duration-[500ms] xl:hover:scale-100 shadow-shadow1 grayscale hover:grayscale-0' width={300} height={240} alt={titulo} src={imagen}/>
+    <Link  className='w-[300px]  h-auto ' href={link}>
+    <Image className='w-[300px]  h-auto xl:scale-90 transition-all duration-[500ms] xl:hover:scale-100 shadow-shadow1 grayscale hover:grayscale-0' width={300} height={240} alt={titulo} src={imagen}/>
 
     </Link>
   )
