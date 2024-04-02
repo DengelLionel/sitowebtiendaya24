eb2285176b83134a1a8b54b04b582fc65ae5d1bf
DengelLionel <dengelriveracajincho@gmail.com>
2024-04-01 21:50:22 -0500

    ok

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -13,6 +13,7 @@ export default function App({ Component, pageProps }: AppProps) {
   TiktokPixel.init( 'CJP96PJC77U1G7J3MO8G' )
 /* 	TiktokPixel.track('ClickButton') */
   return <> <Head> <meta name="facebook-domain-verification" content="esfie7810pj6f3jpp8wjwmtzzludkc" />
+  <meta name="msvalidate.01" content="365EC231D338431AED737B1ED398A42C" />
  
  </Head><main><SiteContext><Component {...pageProps} /></SiteContext></main></>
 }
